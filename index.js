4eaf23b757b8f3570501084074233f2a57f6c2fe
bforbiggy <andyhecsp@gmail.com>
2023-12-28 19:46:24 -0500

    YESSIIIRRRRR

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const express = express()
+const fetch = require("node-fetch")
+const express = require("express")
 const app = express()
-const port = 3000
+const port = 5000
 
-app.get('/', (req, res) => {
-	res.send('Hello World!')
+app.get('/', async (req, res) => {
+	const response = await fetch(`https://api.api-ninjas.com/v1/city?name=${req.query.name}`, {
+		headers: {
+			'X-Api-Key': '1RQN5gMhzkngEsPloobrbw==Ij8wn0TLPC8zPIsr'
+		}
+	});
+	const data = await response.json();
+	res.json(data);
 })
 
 app.listen(port, () => {
